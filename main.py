ac82de7b89a075674dc3116e950617fa0599961d
gvivier <gvivier03@gmail.com>
2023-05-01 19:14:50 +0200

    Mise à jour pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,51 +1,49 @@
-Dist_0 = 0
-dist_180 = 0
+Dist_30 = 0
+dist_150 = 0
 Distance = 0
 pins.servo_write_pin(AnalogPin.P2, 90)
 
 def on_forever():
-    global Distance, dist_180, Dist_0
+    global Distance, dist_150, Dist_30
     Distance = sonar.ping(DigitalPin.P1, DigitalPin.P0, PingUnit.CENTIMETERS)
-    basic.show_number(Distance)
     if Distance < 15:
         kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR1,
             kitronik_klip_motor.MotorDirection.REVERSE,
-            50)
+            30)
         kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR2,
             kitronik_klip_motor.MotorDirection.REVERSE,
-            50)
+            30)
         basic.pause(1000)
         kitronik_klip_motor.motor_off(kitronik_klip_motor.Motors.MOTOR1)
         kitronik_klip_motor.motor_off(kitronik_klip_motor.Motors.MOTOR2)
         basic.pause(500)
-        pins.servo_write_pin(AnalogPin.P2, 180)
+        pins.servo_write_pin(AnalogPin.P2, 150)
         basic.pause(500)
-        dist_180 = sonar.ping(DigitalPin.P1, DigitalPin.P0, PingUnit.CENTIMETERS)
+        dist_150 = sonar.ping(DigitalPin.P1, DigitalPin.P0, PingUnit.CENTIMETERS)
         basic.pause(500)
-        pins.servo_write_pin(AnalogPin.P2, 0)
+        pins.servo_write_pin(AnalogPin.P2, 30)
         basic.pause(500)
-        Dist_0 = sonar.ping(DigitalPin.P1, DigitalPin.P0, PingUnit.CENTIMETERS)
+        Dist_30 = sonar.ping(DigitalPin.P1, DigitalPin.P0, PingUnit.CENTIMETERS)
         basic.pause(500)
-        if Dist_0 < dist_180:
-            kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR1,
-                kitronik_klip_motor.MotorDirection.FORWARD,
-                80)
-            basic.pause(1000)
-            kitronik_klip_motor.motor_off(kitronik_klip_motor.Motors.MOTOR1)
-        else:
+        pins.servo_write_pin(AnalogPin.P2, 90)
+        if Dist_30 < dist_150:
             kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR2,
                 kitronik_klip_motor.MotorDirection.FORWARD,
-                80)
-            basic.pause(1000)
+                50)
+            basic.pause(500)
             kitronik_klip_motor.motor_off(kitronik_klip_motor.Motors.MOTOR2)
-        basic.show_icon(IconNames.NO)
+        else:
+            kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR1,
+                kitronik_klip_motor.MotorDirection.FORWARD,
+                50)
+            basic.pause(500)
+            kitronik_klip_motor.motor_off(kitronik_klip_motor.Motors.MOTOR1)
     else:
         kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR1,
             kitronik_klip_motor.MotorDirection.FORWARD,
-            80)
+            50)
         kitronik_klip_motor.motor_on(kitronik_klip_motor.Motors.MOTOR2,
             kitronik_klip_motor.MotorDirection.FORWARD,
-            80)
-        basic.show_icon(IconNames.YES)
+            50)
         basic.pause(1000)
 basic.forever(on_forever)
